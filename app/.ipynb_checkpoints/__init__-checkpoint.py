822e649f2e7b8aafe1bc6f56cb47e2c8e327a8a1
Varun Gangadharan <varunpg2@illinois.edu>
2024-04-26 23:29:49 -0500

    bruh

--- app/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__, template_folder='../templates/folder')
-    print("Creating app...")
-    # Import routes after app creation to avoid circular imports
-    from app import routes
-
-    return app
